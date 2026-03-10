21d01631e457f25999d37a535979eecc79410b3a
justyaviz <justyaviz@gmail.com>
2026-03-10 12:51:11 +0500

    Update user.py

--- app/keyboards/user.py
+++ app/keyboards/user.py
@@ -1,1 +1,22 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
+menu = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text="👥 Do‘stlarni taklif qilish"),
+            KeyboardButton(text="🏆 Reyting")
+        ],
+        [
+            KeyboardButton(text="🎲 Random o‘yin"),
+            KeyboardButton(text="💎 Mening ballarim")
+        ],
+        [
+            KeyboardButton(text="🎁 Sovg‘alar"),
+            KeyboardButton(text="📘 Qo‘llanma")
+        ],
+        [
+            KeyboardButton(text="📞 Bog‘lanish")
+        ]
+    ],
+    resize_keyboard=True
+)
