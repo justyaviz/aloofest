f7f939925411579f44366a90600d31a457b5a839
justyaviz <justyaviz@gmail.com>
2026-03-10 13:12:18 +0500

    Update random_service.py

--- app/services/random_service.py
+++ app/services/random_service.py
@@ -3,16 +3,16 @@ import aiosqlite
 from app.database.db import DB
 
 
-async def choose_random_winner():
+async def random_winner():
 
     async with aiosqlite.connect(DB) as db:
 
-        cursor = await db.execute("""
+        cur = await db.execute("""
         SELECT telegram_id FROM users
         WHERE referrals_count >= 3
         """)
 
-        users = await cursor.fetchall()
+        users = await cur.fetchall()
 
         if not users:
             return None
