6c48919798e24d264ca14d9a9ebd7b21f6b2349f
justyaviz <justyaviz@gmail.com>
2026-03-10 12:15:51 +0500

    Update db.py

--- app/database/db.py
+++ app/database/db.py
@@ -1,26 +1,26 @@
 import aiosqlite
 
-DB_PATH = "aloofest.db"
+DB = "aloofest.db"
 
 
 async def init_db():
 
-    async with aiosqlite.connect(DB_PATH) as db:
+    async with aiosqlite.connect(DB) as db:
 
         await db.execute("""
-        CREATE TABLE IF NOT EXISTS users (
-            telegram_id INTEGER PRIMARY KEY,
-            name TEXT,
-            username TEXT,
-            instagram TEXT,
-            region TEXT,
-            district TEXT,
-            fest_id TEXT,
-            referrer_id INTEGER,
-            referrals_count INTEGER DEFAULT 0,
-            points INTEGER DEFAULT 0,
-            is_registered INTEGER DEFAULT 0,
-            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        CREATE TABLE IF NOT EXISTS users(
+        telegram_id INTEGER PRIMARY KEY,
+        name TEXT,
+        username TEXT,
+        instagram TEXT,
+        region TEXT,
+        district TEXT,
+        fest_id TEXT,
+        referrer_id INTEGER,
+        referrals_count INTEGER DEFAULT 0,
+        points INTEGER DEFAULT 0,
+        is_registered INTEGER DEFAULT 0,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
         )
         """)
 
