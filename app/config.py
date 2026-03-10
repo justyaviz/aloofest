03b1551aaeb1ccecaffe0de58a3da0558699c97d
justyaviz <justyaviz@gmail.com>
2026-03-10 12:54:34 +0500

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,12 +1,9 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-ADMIN_IDS = [int(x) for x in os.getenv("ADMIN_IDS", "").split(",") if x]
+ADMIN_IDS = [int(x) for x in os.getenv("ADMIN_IDS","").split(",") if x]
 
 CHANNEL = "@aloo_uzb"
 
-PORT = int(os.getenv("PORT", 8080))
+BOT_USERNAME = "aloofest_bot"
