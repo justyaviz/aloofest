afd2bec3d8ecb902813b5ad3168292f886cde516
justyaviz <justyaviz@gmail.com>
2026-03-10 12:11:53 +0500

    Implement main function to start bot and server

--- main.py
+++ main.py
@@ -1,1 +1,36 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.enums import ParseMode
+from aiogram.client.default import DefaultBotProperties
 
+from app.config import BOT_TOKEN
+from app.handlers.start import router as start_router
+from app.handlers.menu import router as menu_router
+from app.handlers.support import router as support_router
+
+from app.database.db import init_db
+from app.web.server import start_web_server
+
+
+async def main():
+
+    bot = Bot(
+        token=BOT_TOKEN,
+        default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+    )
+
+    dp = Dispatcher()
+
+    dp.include_router(start_router)
+    dp.include_router(menu_router)
+    dp.include_router(support_router)
+
+    await init_db()
+
+    await start_web_server()
+
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
