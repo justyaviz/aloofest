731a71c98491dedd704ba2c2f76789314e17b6b0
justyaviz <justyaviz@gmail.com>
2026-03-10 12:15:20 +0500

    Add referral and admin routers to main.py

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ from aiogram.enums import ParseMode
 from aiogram.client.default import DefaultBotProperties
 
 from app.config import BOT_TOKEN
+from app.database.db import init_db
 from app.handlers.start import router as start_router
 from app.handlers.menu import router as menu_router
 from app.handlers.support import router as support_router
-
-from app.database.db import init_db
+from app.handlers.referral import router as referral_router
+from app.handlers.admin import router as admin_router
 from app.web.server import start_web_server
 
 
@@ -23,7 +24,9 @@ async def main():
 
     dp.include_router(start_router)
     dp.include_router(menu_router)
+    dp.include_router(referral_router)
     dp.include_router(support_router)
+    dp.include_router(admin_router)
 
     await init_db()
 
