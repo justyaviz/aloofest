f8beb9c2563da952e9cd369e634f948eaeea557c
justyaviz <justyaviz@gmail.com>
2026-03-10 12:56:14 +0500

    Update start.py

--- app/handlers/start.py
+++ app/handlers/start.py
@@ -1,6 +1,6 @@
 from aiogram import Router
-from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.filters import CommandStart
+from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 router = Router()
 
@@ -13,68 +13,28 @@ async def start(message: Message):
 
 🎯 Sizda 2 xil yutish imkoniyati bor:
 
-1️⃣ TOP 3 ga kirish
+1️⃣ TOP 3 ga kirish  
 2️⃣ Random o'yini
 
-🥇 Tecno Spark GO 30C
-🥈 Mini pech Artel
+🎁 <b>TOP sovg‘alar</b>
+
+🥇 Tecno Spark GO 30C  
+🥈 Mini pech Artel  
 🥉 Ryugzak
 
-🎲 Random sovg‘alar:
+🎲 <b>Random sovg‘alar</b>
+
 Airpods Max Copy (3 ta)
 
 📅 G'oliblar JONLI EFIR orqali aniqlanadi.
 
-1️⃣ Birinchi qadam: ISHTIROK ETAMAN
+1️⃣ Birinchi qadam: <b>ISHTIROK ETAMAN</b>
 """
 
     kb = InlineKeyboardMarkup(
         inline_keyboard=[
-            [
-                InlineKeyboardButton(
-                    text="🚀 ISHTIROK ETAMAN",
-                    callback_data="join_contest"
-                )
-            ]
+            [InlineKeyboardButton(text="🚀 ISHTIROK ETAMAN", callback_data="join")]
         ]
     )
 
     await message.answer(text, reply_markup=kb)
-
-from aiogram.types import CallbackQuery
-from app.keyboards.user import menu
-
-
-@router.callback_query(lambda c: c.data == "join_contest")
-async def join_contest(callback: CallbackQuery):
-
-    text = """
-🏆 <b>aloofest konkurs tizimi</b>
-
-🎯 Ball tizimi
-
-1 ta do‘st taklif qilish = <b>5 ball</b>
-
-🎲 Random o‘yini
-
-Kamida <b>3 ta do‘st</b> taklif qilish kerak.
-
-3 referal = 15 ball
-
-🥇 TOP konkurs
-
-Eng ko‘p ball yig‘ganlar yutadi.
-
-📋 Shartlar
-
-1️⃣ @aloo_uzb kanaliga obuna bo‘lish
-
-2️⃣ Instagram:
-@aloo.uz_ sahifasiga obuna
-
-🌐 Ro‘yxatdan o‘tish WEB orqali amalga oshiriladi.
-"""
-
-    await callback.message.answer(text, reply_markup=menu)
-
-    await callback.answer()
