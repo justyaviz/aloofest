32b90d75a09655cf81589216a99fa06ffa8adc80
justyaviz <justyaviz@gmail.com>
2026-03-10 12:12:03 +0500

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,1 +1,12 @@
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+ADMIN_IDS = [int(x) for x in os.getenv("ADMIN_IDS", "").split(",")]
+
+CHANNELS = os.getenv("CHANNELS", "").split(",")
+
+PORT = int(os.getenv("PORT", 8080))
