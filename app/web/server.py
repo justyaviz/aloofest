e72044aeacaf26e1ddd65c104e56956de9f63559
justyaviz <justyaviz@gmail.com>
2026-03-10 13:13:05 +0500

    Update server.py

--- app/web/server.py
+++ app/web/server.py
@@ -1,27 +1,22 @@
 from aiohttp import web
 import os
-import asyncio
 
-PORT = int(os.getenv("PORT", 8080))
+PORT = int(os.environ.get("PORT",8080))
 
 
 async def health(request):
     return web.Response(text="OK")
 
 
-async def start_web_server():
+async def start_web():
 
     app = web.Application()
+
     app.router.add_get("/health", health)
 
     runner = web.AppRunner(app)
     await runner.setup()
 
-    site = web.TCPSite(runner, "0.0.0.0", PORT)
+    site = web.TCPSite(runner,"0.0.0.0",PORT)
+
     await site.start()
-
-    print(f"WEB SERVER RUNNING ON PORT {PORT}")
-
-    # serverni tirik ushlab turish
-    while True:
-        await asyncio.sleep(3600)
