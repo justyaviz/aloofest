3f8938a5e0f72ce84bbc756bfa7bba0b21b4b20f
justyaviz <justyaviz@gmail.com>
2026-03-10 13:19:48 +0500

    Update routes.py

--- app/web/routes.py
+++ app/web/routes.py
@@ -1,29 +1,70 @@
 from aiohttp import web
 import aiosqlite
+
 from app.database.db import DB
-from app.utils.helpers import generate_fest_id
+
+routes = web.RouteTableDef()
 
 
-async def register(request):
+@routes.get("/health")
+async def health(request):
+    return web.Response(text="OK")
+
+
+@routes.get("/register")
+async def register_page(request):
+
+    return web.FileResponse(
+        "app/web/templates/register.html"
+    )
+
+
+@routes.post("/register")
+async def register_user(request):
 
     data = await request.post()
 
+    telegram_id = data["telegram_id"]
     name = data["name"]
     instagram = data["instagram"]
     region = data["region"]
     district = data["district"]
-    user_id = int(data["user_id"])
-
-    fest = generate_fest_id()
 
     async with aiosqlite.connect(DB) as db:
 
+        cur = await db.execute(
+        "SELECT COUNT(*) FROM users"
+        )
+
+        count = (await cur.fetchone())[0] + 1
+
+        fest_id = f"FEST-{count:03d}"
+
         await db.execute("""
-        UPDATE users
-        SET name=?,instagram=?,region=?,district=?,fest_id=?,is_registered=1,points=5
-        WHERE telegram_id=?
-        """,(name,instagram,region,district,fest,user_id))
+
+        INSERT INTO users(
+        telegram_id,
+        name,
+        instagram,
+        region,
+        district,
+        fest_id,
+        registered
+        )
+
+        VALUES(?,?,?,?,?,?,1)
+
+        """,
+
+        (telegram_id,name,instagram,region,district,fest_id)
+
+        )
 
         await db.commit()
 
-    return web.Response(text="OK")
+    return web.Response(text="Registered successfully")
+
+
+def setup_routes(app):
+
+    app.add_routes(routes)
