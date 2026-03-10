a2d1369c24091e992e1fa904e3661048ee07df70
justyaviz <justyaviz@gmail.com>
2026-03-10 12:12:28 +0500

    Update server.py

--- app/web/server.py
+++ app/web/server.py
@@ -1,1 +1,20 @@
+from aiohttp import web
+from app.config import PORT
 
+
+async def health(request):
+    return web.Response(text="OK")
+
+
+async def start_web_server():
+
+    app = web.Application()
+
+    app.router.add_get("/health", health)
+
+    runner = web.AppRunner(app)
+    await runner.setup()
+
+    site = web.TCPSite(runner, "0.0.0.0", PORT)
+
+    await site.start()
