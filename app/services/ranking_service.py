f7c958002a4dbc5f4d13cea0b4a82bf92a2ca8ec
justyaviz <justyaviz@gmail.com>
2026-03-10 13:11:43 +0500

    Update ranking_service.py

--- app/services/ranking_service.py
+++ app/services/ranking_service.py
@@ -2,15 +2,15 @@ import aiosqlite
 from app.database.db import DB
 
 
-async def get_top_users():
+async def get_top():
 
     async with aiosqlite.connect(DB) as db:
 
-        cursor = await db.execute("""
+        cur = await db.execute("""
         SELECT name, fest_id, points
         FROM users
         ORDER BY points DESC
         LIMIT 10
         """)
 
-        return await cursor.fetchall()
+        return await cur.fetchall()
