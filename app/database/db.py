5b2e0bd62f8c5a0d6523b1d5f4db2b6fb9c83ec0
justyaviz <justyaviz@gmail.com>
2026-03-10 12:46:11 +0500

    Refactor database setup and add users table
    Refactor database initialization to create directory and use a formatted database path. Added users table creation if it doesn't exist.

--- app/database/db.py
+++ app/database/db.py
@@ -1,15 +1,32 @@
 import aiosqlite
-from .models import USERS_TABLE, WINNERS_TABLE, BROADCAST_TABLE
+import os
 
-DB = "data/aloofest.db"
+DB_DIR = "data"
+DB_PATH = f"{DB_DIR}/aloofest.db"
 
 
 async def init_db():
 
-    async with aiosqlite.connect(DB) as db:
+    # data papkasini yaratamiz
+    os.makedirs(DB_DIR, exist_ok=True)
 
-        await db.execute(USERS_TABLE)
-        await db.execute(WINNERS_TABLE)
-        await db.execute(BROADCAST_TABLE)
+    async with aiosqlite.connect(DB_PATH) as db:
+
+        await db.execute("""
+        CREATE TABLE IF NOT EXISTS users(
+        telegram_id INTEGER PRIMARY KEY,
+        name TEXT,
+        username TEXT,
+        instagram TEXT,
+        region TEXT,
+        district TEXT,
+        fest_id TEXT,
+        referrer_id INTEGER,
+        referrals_count INTEGER DEFAULT 0,
+        points INTEGER DEFAULT 0,
+        is_registered INTEGER DEFAULT 0,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )
+        """)
 
         await db.commit()
